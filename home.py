626209c239e40bcfdd4b56301cc403959fb6a40a
1quest <aleks.senek@gmail.com>
2022-12-30 15:47:03 +0100

    test

--- home.py
+++ home.py
@@ -56,16 +56,16 @@ def future():
                            table=table_apts)
 
 
-@app.route('/get_areas', methods=['POST', 'GET'])
-def areas():
-    df = load_data()
+@app.route('/get_future_areas', methods=['POST', 'GET'])
+def future_areas():
+    df = load_future_data()
     locations = df["location"].str.title()
     return json.dumps(sorted(dict(locations.drop_duplicates()).values()))
 
 
-@app.route('/get_future_areas', methods=['POST', 'GET'])
-def future_areas():
-    df = load_future_data()
+@app.route('/get_areas', methods=['POST', 'GET'])
+def areas():
+    df = load_data()
     locations = df["location"].str.title()
     return json.dumps(sorted(dict(locations.drop_duplicates()).values()))
 
