fbb5c6a9d5bd7dd528af4b5689af8166c0a5699c
1quest <aleks.senek@gmail.com>
2022-12-30 16:07:55 +0100

    whytyyy

--- home.py
+++ home.py
@@ -58,9 +58,10 @@ def future():
 
 @app.route('/get_futureareas', methods=['POST', 'GET'])
 def future_areas():
-    df = load_future_data()
-    locations = df["location"].str.title()
-    return "WHAAAGGAGA"# json.dumps(sorted(dict(locations.drop_duplicates()).values()))
+    #df = load_future_data()
+    #locations = df["location"].str.title()
+    # json.dumps(sorted(dict(locations.drop_duplicates()).values()))
+    return "WHAAAGGAGA"
 
 
 @app.route('/get_areas', methods=['POST', 'GET'])
